a561436c5bd751fb6e256dc0374354214ad63f9f
Ren Yuzuriha <xanxys@gmail.com>
2023-05-10 03:45:26 +0900

    Print verif stats

--- src/main.ts
+++ src/main.ts
@@ -290,21 +290,29 @@ let codeMirror = CodeMirror(document.body, {
 });
 
 
-fetch("/demo0.mm") // maxNest 1
-    //fetch("/big-unifier.mm") // maxNest 1
+//fetch("/demo0.mm") // maxNest 1
+//fetch("/big-unifier.mm") // maxNest 1
     //fetch("/set.mm") // maxNest 5
     //fetch("/iset.mm") // maxNest 4
-    //fetch("/hol.mm") // maxNest 3
+    fetch("/hol.mm") // maxNest 3
     .then((response) => response.text())
     .then((text) => {
         //        codeMirror.setValue(text);
         const ast = parseMM(text);
         const db = organizeMM(ast);
+        let numCheckedProof = 0;
+        let numVerifiedProof = 0;
         for (const [label, frame] of db.extFrames.entries()) {
             console.log("ExtFrame", label, frame);
             if (!frame.proofLabels) {
                 continue;
             }
-            console.log("->", verifyProof(db, frame));
+            numCheckedProof++;
+            const verifResult = verifyProof(db, frame);
+            console.log("->", verifResult);
+            if (verifResult === true) {
+                numVerifiedProof++;
+            }
         }
+        console.log("verification result", numVerifiedProof, "/", numCheckedProof);
     });
