1929d10a84aead0cef0c82e4afbb9985f6f2f77d
Ren Yuzuriha <xanxys@gmail.com>
2023-05-10 00:37:52 +0900

    proof verifier WIP

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { parseMM, DStmt, FStmt, EStmt, MMBlock, EntryType, CStmt, VStmt, AStmt, PStmt } from "./parser";
 
-type ExtFrame = {
+type FrameContext = {
     disjoints: DStmt[],
     varHyps: FStmt[],
     logiHyps: EStmt[],
+};
+
+function cloneFrameContext(ctx: FrameContext): FrameContext {
+    return {
+        disjoints: Array.from(ctx.disjoints),
+        varHyps: Array.from(ctx.varHyps),
+        logiHyps: Array.from(ctx.logiHyps),
+    };
+}
+
+type ExtFrame = {
+    context: FrameContext,
 
     assertionLabel: string,
     assertionTypecode: string,
@@ -42,20 +54,8 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
         extFrames: new Map(),
     };
 
-    function procBlock(block: MMBlock, outermost: boolean) {
-        let currFrame: ExtFrame = {
-            disjoints: [],
-            varHyps: [],
-            logiHyps: [],
-            assertionLabel: "",
-            assertionTypecode: "",
-            assertionSymbols: [],
-            assertionLine: 0,
-            optionalDisjoints: [],
-            optionalVarHyps: [],
-            proofLabels: null,
-            proofCompressed: null,
-        };
+    function procBlock(block: MMBlock, outermost: boolean, outerCtx: FrameContext) {
+        let currCtx = cloneFrameContext(outerCtx);
 
         for (const ent of block.entries) {
             if (ent.entryTy === EntryType.CS) {
@@ -69,60 +69,41 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
                 stmt.symbols.forEach((s) => db.varSymbols.add(s));
             } else if (ent.entryTy === EntryType.FS) {
                 const stmt = ent.stmt as FStmt;
-                currFrame.varHyps.push(stmt);
+                currCtx.varHyps.push(stmt);
             } else if (ent.entryTy === EntryType.ES) {
                 const stmt = ent.stmt as EStmt;
-                currFrame.logiHyps.push(stmt);
+                currCtx.logiHyps.push(stmt);
             } else if (ent.entryTy === EntryType.DS) {
                 const stmt = ent.stmt as DStmt;
-                currFrame.disjoints.push(stmt);
+                currCtx.disjoints.push(stmt);
             } else if (ent.entryTy === EntryType.AS) {
                 const stmt = ent.stmt as AStmt;
-                currFrame.assertionLabel = stmt.label;
-                currFrame.assertionLine = stmt.declLine;
-                currFrame.assertionTypecode = stmt.typecode;
-                currFrame.assertionSymbols = stmt.symbols;
-                db.extFrames.set(stmt.label, currFrame);
-
-                currFrame = {
-                    disjoints: [],
-                    varHyps: [],
-                    logiHyps: [],
-                    assertionLabel: "",
-                    assertionTypecode: "",
-                    assertionSymbols: [],
-                    assertionLine: 0,
+                db.extFrames.set(stmt.label, {
+                    context: cloneFrameContext(currCtx),
+                    assertionLabel: stmt.label,
+                    assertionLine: stmt.declLine,
+                    assertionTypecode: stmt.typecode,
+                    assertionSymbols: stmt.symbols,
                     optionalDisjoints: [],
                     optionalVarHyps: [],
                     proofLabels: null,
                     proofCompressed: null,
-                };
+                });
             } else if (ent.entryTy === EntryType.PS) {
                 const stmt = ent.stmt as PStmt;
-                currFrame.assertionLabel = stmt.label;
-                currFrame.assertionLine = stmt.declLine;
-                currFrame.assertionTypecode = stmt.typecode;
-                currFrame.assertionSymbols = stmt.symbols;
-
-                currFrame.proofLabels = stmt.proofLabels;
-                currFrame.proofCompressed = stmt.proofCompressed;
-                db.extFrames.set(stmt.label, currFrame);
-
-                currFrame = {
-                    disjoints: [],
-                    varHyps: [],
-                    logiHyps: [],
-                    assertionLabel: "",
-                    assertionTypecode: "",
-                    assertionSymbols: [],
-                    assertionLine: 0,
+                db.extFrames.set(stmt.label, {
+                    context: cloneFrameContext(currCtx),
+                    assertionLabel: stmt.label,
+                    assertionLine: stmt.declLine,
+                    assertionTypecode: stmt.typecode,
+                    assertionSymbols: stmt.symbols,
                     optionalDisjoints: [],
                     optionalVarHyps: [],
-                    proofLabels: null,
-                    proofCompressed: null,
-                };
+                    proofLabels: stmt.proofLabels,
+                    proofCompressed: stmt.proofCompressed,
+                });
             } else if (ent.entryTy === EntryType.Block) {
-                procBlock(ent.block as MMBlock, false);
+                procBlock(ent.block as MMBlock, false, currCtx);
             } else {
                 console.log("Not implemented", ent);
                 throw new Error("Not implemented: " + ent.entryTy);
@@ -130,14 +111,51 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
         }
     }
 
-    procBlock(outermostBlock, true);
+    procBlock(outermostBlock, true, {
+        disjoints: [],
+        varHyps: [],
+        logiHyps: [],
+    });
     return db;
 }
 
+function printStack(stack: any[]) {
+    console.log("====stack", ...stack);
+}
+
 // Returns true is the proof is valid.
-function verifyProof(db: MMDB, frame: ExtFrame) : boolean {
+function verifyProof(db: MMDB, frame: ExtFrame): boolean {
     if (!frame.proofLabels) {
         throw new Error("frame must contain a proof");
+    }
+
+    const stack: any[] = [];
+    for (const label of frame.proofLabels) {
+        const varHyp = frame.context.varHyps.filter((h) => h.label === label)[0];
+        printStack(stack);
+        console.log("applying", label);
+        if (varHyp) {
+            console.log("ref-var-hyp", varHyp);
+            stack.push([varHyp.typecode, varHyp.symbol]);
+            continue;
+        }
+        const logiHyp = frame.context.logiHyps.filter((h) => h.label === label)[0];
+        if (logiHyp) {
+            console.log("ref-logi-hyp", logiHyp);
+            stack.push([logiHyp.typecode, ...logiHyp.symbols]);
+            continue;
+        }
+        const assertion = db.extFrames.get(label);
+        if (assertion) {
+            console.log("ref-assertion", assertion);
+            console.log("Mand hypotheses", assertion.context);
+            // TODO: this is currently all hypothesis, not mandatory hypos.
+            // need to trim down to mandatory hypothesis before trying to apply unification.
+            stack.push([assertion.assertionTypecode, ...assertion.assertionSymbols]);
+            continue;
+        }
+
+        throw new Error(`Invalid frame, missing ${label}`);
     }
 
     return false;
@@ -151,19 +169,20 @@ let codeMirror = CodeMirror(document.body, {
 
 
 fetch("/demo0.mm") // maxNest 1
-//fetch("/big-unifier.mm") // maxNest 1
-//fetch("/set.mm") // maxNest 5
+    //fetch("/big-unifier.mm") // maxNest 1
+    //fetch("/set.mm") // maxNest 5
     //fetch("/iset.mm") // maxNest 4
-//fetch("/hol.mm") // maxNest 3
+    //fetch("/hol.mm") // maxNest 3
     .then((response) => response.text())
     .then((text) => {
         //        codeMirror.setValue(text);
         const ast = parseMM(text);
         const db = organizeMM(ast);
         for (const [label, frame] of db.extFrames.entries()) {
+            console.log("ExtFrame", label, frame);
             if (!frame.proofLabels) {
                 continue;
             }
-            console.log(label, frame, verifyProof(db, frame));
+            console.log("->", verifyProof(db, frame));
         }
     });
