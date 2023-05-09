338257db1d2c5747fb2bf018f02dd58daa19326c
Ren Yuzuriha <xanxys@gmail.com>
2023-05-10 03:42:28 +0900

    Successful unification for demo0.mm

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { parseMM, DStmt, FStmt, EStmt, MMBlock, EntryType, CStmt, VStmt, AStmt, 
 
 type FrameContext = {
     disjoints: DStmt[],
+    hyps: Hypothesis[],
     varHyps: FStmt[],
     logiHyps: EStmt[],
 };
@@ -9,10 +10,19 @@ type FrameContext = {
 function cloneFrameContext(ctx: FrameContext): FrameContext {
     return {
         disjoints: Array.from(ctx.disjoints),
+        hyps: Array.from(ctx.hyps),
         varHyps: Array.from(ctx.varHyps),
         logiHyps: Array.from(ctx.logiHyps),
     };
 }
+
+// Either FStmt or EStmt
+type Hypothesis = {
+    label: string,
+    typecode: string,
+    symbols: string[],
+    isLogi: boolean,
+};
 
 type ExtFrame = {
     context: FrameContext,
@@ -22,8 +32,7 @@ type ExtFrame = {
     assertionSymbols: string[],
     assertionLine: number,
 
-    optionalDisjoints: DStmt[],
-    optionalVarHyps: FStmt[],
+    mandatoryHyps: Hypothesis[],
     proofLabels: string[] | null,
     proofCompressed: string | null,
 };
@@ -44,6 +53,23 @@ class ASTError {
         this.line = line;
         this.message = message;
     }
+}
+
+function filterMandatoryHyps(hyps: Hypothesis[], db: MMDB, logiHyps: EStmt[], assertionSymbols: string[]): Hypothesis[] {
+    const usedVars = new Set<string>();
+    for (const logiHyp of logiHyps) {
+        for (const symbol of logiHyp.symbols) {
+            if (db.varSymbols.has(symbol)) {
+                usedVars.add(symbol);
+            }
+        }
+    }
+    for (const symbol of assertionSymbols) {
+        if (db.varSymbols.has(symbol)) {
+            usedVars.add(symbol);
+        }
+    }
+    return hyps.filter((hyp) => hyp.isLogi || usedVars.has(hyp.symbols[0]));
 }
 
 // throws ASTError.
@@ -70,9 +96,21 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
             } else if (ent.entryTy === EntryType.FS) {
                 const stmt = ent.stmt as FStmt;
                 currCtx.varHyps.push(stmt);
+                currCtx.hyps.push({
+                    label: stmt.label,
+                    typecode: stmt.typecode,
+                    symbols: [stmt.symbol],
+                    isLogi: false,
+                });
             } else if (ent.entryTy === EntryType.ES) {
                 const stmt = ent.stmt as EStmt;
                 currCtx.logiHyps.push(stmt);
+                currCtx.hyps.push({
+                    label: stmt.label,
+                    typecode: stmt.typecode,
+                    symbols: stmt.symbols,
+                    isLogi: true,
+                });
             } else if (ent.entryTy === EntryType.DS) {
                 const stmt = ent.stmt as DStmt;
                 currCtx.disjoints.push(stmt);
@@ -84,8 +122,7 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
                     assertionLine: stmt.declLine,
                     assertionTypecode: stmt.typecode,
                     assertionSymbols: stmt.symbols,
-                    optionalDisjoints: [],
-                    optionalVarHyps: [],
+                    mandatoryHyps: filterMandatoryHyps(currCtx.hyps, db, currCtx.logiHyps, stmt.symbols),
                     proofLabels: null,
                     proofCompressed: null,
                 });
@@ -97,8 +134,7 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
                     assertionLine: stmt.declLine,
                     assertionTypecode: stmt.typecode,
                     assertionSymbols: stmt.symbols,
-                    optionalDisjoints: [],
-                    optionalVarHyps: [],
+                    mandatoryHyps: filterMandatoryHyps(currCtx.hyps, db, currCtx.logiHyps, stmt.symbols),
                     proofLabels: stmt.proofLabels,
                     proofCompressed: stmt.proofCompressed,
                 });
@@ -113,23 +149,36 @@ function organizeMM(outermostBlock: MMBlock): MMDB {
 
     procBlock(outermostBlock, true, {
         disjoints: [],
+        hyps: [],
         varHyps: [],
         logiHyps: [],
     });
     return db;
 }
 
-function printStack(stack: any[]) {
-    console.log("====stack", ...stack);
+function printStack(stack: string[][]) {
+    console.log("====stack", stack.map((syms) => syms.join(" ")));
 }
 
-// Returns true is the proof is valid.
-function verifyProof(db: MMDB, frame: ExtFrame): boolean {
+function symSeqEqual(symSeqA: string[], symSeqB: string[]): boolean {
+    if (symSeqA.length !== symSeqB.length) {
+        return false;
+    }
+    for (let i = 0; i < symSeqA.length; i++) {
+        if (symSeqA[i] !== symSeqB[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Returns true is the proof is valid, otherwise failure reason string.
+function verifyProof(db: MMDB, frame: ExtFrame): true | string {
     if (!frame.proofLabels) {
         throw new Error("frame must contain a proof");
     }
 
-    const stack: any[] = [];
+    const stack: string[][] = [];
     for (const label of frame.proofLabels) {
         const varHyp = frame.context.varHyps.filter((h) => h.label === label)[0];
         printStack(stack);
@@ -147,18 +196,91 @@ function verifyProof(db: MMDB, frame: ExtFrame): boolean {
         }
         const assertion = db.extFrames.get(label);
         if (assertion) {
-            console.log("ref-assertion", assertion);
-            console.log("Mand hypotheses", assertion.context);
-            // TODO: this is currently all hypothesis, not mandatory hypos.
-            // need to trim down to mandatory hypothesis before trying to apply unification.
-            stack.push([assertion.assertionTypecode, ...assertion.assertionSymbols]);
+            const arity = assertion.mandatoryHyps.length;
+            console.log("ref-assertion", "arity", arity, assertion);
+            if (arity === 0) {
+                stack.push([assertion.assertionTypecode, ...assertion.assertionSymbols]);
+                continue;
+            }
+
+            if (stack.length < arity) {
+                return "Unification failed";
+            }
+
+            const args: string[][] = [];
+            for (let i = 0; i < arity; i++) {
+                args.push(stack.pop() as string[]);
+            }
+            args.reverse();
+
+            const hyps = assertion.mandatoryHyps;
+            console.log("Trying to unify", hyps, "->", args);
+            const unifier = new Map<string, string[]>(); // key:variable, value:symbolSeq
+            for (let i = 0; i < arity; i++) {
+                // TODO: currently doing 1:1 matching, but actually variable can match sequence of symbols+, not just 1 symbol.
+                // since vHyp is [typecode, var], it's trivial to find match.
+                // What about logi hyp...????
+                if (args[i].length < 1 + hyps[i].symbols.length) {
+                    // TODO: is it ok to match var to 0 symbols?
+                    return `Unification failed for hyp(1 typecode + ${hyps[i].symbols.length} symbols) -> ${args[i]}`;
+                }
+                if (args[i][0] !== hyps[i].typecode) {
+                    return `Unification failed: typecode mismatch`;
+                }
+                const newUnification = args[i].slice(1);
+                if (hyps[i].symbols.length === 1) {
+                    const existingUnification = unifier.get(hyps[i].symbols[0]);
+                    if (existingUnification !== undefined && !symSeqEqual(existingUnification, newUnification)) {
+                        return `Unification failed: already assigned unifier ${existingUnification} contradicts newly required unification ${newUnification}`;
+                    }
+                    unifier.set(hyps[i].symbols[0], newUnification);
+                } else {
+                    // "easy" case: everything is already unified.
+                    const matchTrial: string[] = [hyps[i].typecode];
+
+                    hyps[i].symbols.forEach((sym) => {
+                        if (db.varSymbols.has(sym)) {
+                            const existingUnif = unifier.get(sym);
+                            if (existingUnif === undefined) {
+                                throw new Error("Needs generic seq->seq unification, but not implemented");
+                            }
+                            matchTrial.push(...existingUnif);
+                        } else {
+                            matchTrial.push(sym);
+                        }
+                    });
+
+                    if (symSeqEqual(matchTrial, args[i])) {
+                        continue;
+                    } else {
+                        console.log(matchTrial, "!=", args[i]);
+                        return "Unification failed";
+                    }
+                }
+            }
+            console.log(unifier);
+
+            // Push assertion with unifier.
+            const symSeq: string[] = [assertion.assertionTypecode];
+            for (const sym of assertion.assertionSymbols) {
+                if (db.varSymbols.has(sym)) {
+                    const unifiedSyms = unifier.get(sym);
+                    if (unifiedSyms === undefined) {
+                        return `Somehow unifier misses symbol contained in the assertion ${sym}`; // probably bug in the code, not proof
+                    }
+                    symSeq.push(...unifiedSyms);
+                } else {
+                    symSeq.push(sym);
+                }
+            }
+            stack.push(symSeq);
             continue;
         }
 
         throw new Error(`Invalid frame, missing ${label}`);
     }
 
-    return false;
+    return true;
 }
 
 declare const CodeMirror: any;
